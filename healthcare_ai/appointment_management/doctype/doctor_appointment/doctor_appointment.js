3d047df4b33a82858611be1ed868e4f45c3a8e8b
coder1823 <coder.1823@gmail.com>
2024-04-14 22:12:17 +0530

    module updation

--- healthcare_ai/appointment_management/doctype/doctor_appointment/doctor_appointment.js
+++ healthcare_ai/appointment_management/doctype/doctor_appointment/doctor_appointment.js
@@ -3,7 +3,28 @@
 
 frappe.ui.form.on('Doctor Appointment', {
 	refresh: function(frm) {
-		
-
+		frappe.call({
+			method:'healthcare_ai.appointment_management.doctype.doctor_appointment.doctor_appointment.available_doctor_list',
+			callback:function(r){
+			if (r.message) {
+				var doctor_list = r.message;
+				frm.set_query("doctor", function() {
+					return {
+						filters: {
+							name: ['in', doctor_list]
+						}
+					};
+				});
+			}
+			}
+		})
+		if (frm.doc.status == 'Waiting for consultation'){
+			frm.add_custom_button(__("Cancel Appointment"), function() {
+				frm.set_value('status','Cancelled')
+				frm.save()
+			})
+			
+		}
 	}
+
 });
--- healthcare_ai/appointment_management/doctype/doctor_appointment/doctor_appointment.py
+++ healthcare_ai/appointment_management/doctype/doctor_appointment/doctor_appointment.py
@@ -27,7 +27,7 @@ class DoctorAppointment(Document):
 	
 	def generate_token_number(self):
 		setting = frappe.get_single('MedInsights Settings')
-		if setting.token_rest_based_on ==1:
+		if setting.token_rest_based_on ==1 and not self.token_number:
 			if setting.appointment_based_on == 'Ward Number':
 					token_number_query = frappe.db.sql(f"SELECT token_number FROM `tabDoctor Appointment` WHERE ward_number ='{self.ward_number}' and DATE(creation)=CURDATE( ) ORDER BY name DESC LIMIT 1 ",as_dict = True)
 					self.token_number = 1 if not token_number_query else token_number_query[0].token_number + 1
@@ -38,11 +38,34 @@ class DoctorAppointment(Document):
 					token_number_query = frappe.db.sql(f"SELECT token_number FROM `tabDoctor Appointment` WHERE DATE(creation)=CURDATE( ) ORDER BY name DESC LIMIT 1 ",as_dict = True)
 					self.token_number = 1 if not token_number_query else token_number_query[0].token_number + 1
 
+	def create_consultation(self):
+		consultation_doc = frappe.new_doc()
+		consultation_doc.patient_id = self.patient_id
+		consultation_doc.patient_name = self.name1
+		consultation_doc.save()
+
+	def update_patient_record(self):
+		patient_doc = frappe.get_doc('Patient',self.patient_id)
+		patient_doc.append('appointment',{
+			'last_visit':frappe.utils.now_datetime().date(),
+			'token_number':self.token_number,
+			'consultee_id':self.doctor
+		})
+		patient_doc.save()
+		
+
 
 
 	def before_save(self):
 		self.check_alredy_exits()
 		self.generate_token_number()
+  
+	def on_update(self):
+		self.create_consultation()
+		self.update_patient_record()
+
+  
+	
   
   
   
